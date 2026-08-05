66379983ce1694f7744e7f523c79c77eb24113c7
MOJI <apple@apples-MacBook-Air.local>
2026-08-05 10:20:42 +0700

    add (id) code to komik.js

--- models/komik.js
+++ models/komik.js
@@ -1,2 +1,8 @@
 module.exports = {sequelize, DataTypes} => {
     const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        }
