0597759d872a94beb216a5d64a0f0b2dc29afafe
MOJI <apple@apples-MacBook-Air.local>
2026-08-05 10:11:26 +0700

    add id code to genre.js

--- models/genre.js
+++ models/genre.js
@@ -1,2 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
-    
+    const Genre = sequelize.define("Genre", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        }
